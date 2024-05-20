89e1098aebcf313873721453342c631e0efb7d8a
KiwiGin <bayeskiwigen@gmail.com>
2024-05-19 22:23:35 -0500

    chore: Update npm dependencies and add framer-motion library

--- webSenati/src/components/LoginCard.jsx
+++ webSenati/src/components/LoginCard.jsx
@@ -1,18 +1,19 @@
-import { useContext, useState } from 'react'
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/authContext.jsx';
-import coco from '../assets/coco1.jfif'
+import { motion } from 'framer-motion';
+import coco from '../assets/coco1.jfif';
 
 export function LoginCard() {
     const [nombre, setUsername] = useState('');
     const [contraseña, setPassword] = useState('');
-    // const [loggedIn, setLoggedIn] = useState(false);
-    // const [isStudent, setIsStudent] = useState(false);
-    // const [isTeacher, setIsTeacher] = useState(false);
-    const [err, setErr] = useState(null)
+    const [err, setErr] = useState(null);
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [email, setEmail] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     const authContext = useContext(AuthContext);
-    console.log("AuthContext value:", authContext); 
-    const { login } = authContext; 
+    const { login, register } = authContext;
     const navigate = useNavigate();
 
     const handleUsernameChange = (e) => {
@@ -23,51 +24,36 @@ export function LoginCard() {
         setPassword(e.target.value);
     };
 
-    const handleSubmit = async (e) => {
-        //setName(username)
-        e.preventDefault();
-
-        try {
-            await login({ nombre, contraseña })
-            navigate('/home', { state: { usuario: nombre } });
-        } catch (err) {
-            setErr(err.response.data)
-        }
-
-
-
-        
-        console.log('Username:', nombre);
-        console.log('Password:', contraseña);
-        // Se validan credenciales
-        // Se redirige a la página de inicio
-        // if (username === 'estudiante' && password === 'contraseña') {
-        //     // Si las credenciales son válidas, establece el estado loggedIn a true
-        //     setLoggedIn(true);
-        //     setIsStudent(true);
-        // }
-        // if (username === 'docente' && password === 'contraseña') {
-        //     // Si las credenciales son válidas, establece el estado loggedIn a true
-        //     setLoggedIn(true);
-        //     setIsTeacher(true);
-        // }
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
     };
 
-    // if (loggedIn) {
-    //     if (isStudent) {
-    //         return <Navigate to="/home" state={{ usuario: username }} />;
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+    };
 
-    //     }
-    //     if (isTeacher) {
-    //         return <Navigate to="/home" state={{ usuario: username }} />;
-    //     }
-    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (isRegistering) {
+                await register({ nombre, email, contraseña, confirmPassword });
+                navigate('/home', { state: { usuario: nombre } });
+            } else {
+                await login({ nombre, contraseña });
+                navigate('/home', { state: { usuario: nombre } });
+            }
+        } catch (err) {
+            setErr(err.response.data);
+        }
+    };
+
+    const toggleForm = () => {
+        setIsRegistering(!isRegistering);
+        setErr(null);
+    };
 
     return (
-        <>
-
         <div className='flex flex-row items-center justify-center w-full h-full mx-96'>
-
             <div className='flex flex-row text-center bg-gradient-to-b from-yellow-200 from-5% to-green-600 rounded-3xl w-full'>
                 <div className='flex flex-col w-full'>
                     <div className='flex flex-col items-center justify-center m-6 w-full'>
@@ -75,34 +61,60 @@ export function LoginCard() {
                             <img className='object-cover' src="/logococo.svg" alt="" />
                         </div>
 
-                        <h1>INICIO DE SESIÓN</h1>
-                        <br/><br />
-                        <form onSubmit={handleSubmit}>
-                            <label className='flex flex-col'>
-                                Usuario
-                                <input className='rounded-3xl px-4 py-2' type="text" value={nombre} onChange={handleUsernameChange} />
-                            </label>
-                            <br />
-                            <label className='flex flex-col'>
-                                Contraseña
-                                <input className='rounded-3xl px-4 py-2' type="password" value={contraseña} onChange={handlePasswordChange} />
-                            </label>
-                            <br />
-                            <a href="#">¿No recuerda su contraseña?</a>
-                            <br />
-                            {err && err}
-                            <button type="submit" className="rounded-full bg-red-500 text-white px-4 py-2 w-full">Login</button>
-                        </form>
+                        <motion.div
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <h1>{isRegistering ? 'REGISTRO' : 'INICIO DE SESIÓN'}</h1>
+                            <br /><br />
+                            <form onSubmit={handleSubmit}>
+                                {isRegistering && (
+                                    <label className='flex flex-col'>
+                                        Email
+                                        <input className='rounded-3xl px-4 py-2' type="email" value={email} onChange={handleEmailChange} />
+                                    </label>
+                                )}
+                                <br />
+                                <label className='flex flex-col'>
+                                    Usuario
+                                    <input className='rounded-3xl px-4 py-2' type="text" value={nombre} onChange={handleUsernameChange} />
+                                </label>
+                                <br />
+                                <label className='flex flex-col'>
+                                    Contraseña
+                                    <input className='rounded-3xl px-4 py-2' type="password" value={contraseña} onChange={handlePasswordChange} />
+                                </label>
+                                <br />
+                                {isRegistering && (
+                                    <label className='flex flex-col'>
+                                        Confirmar Contraseña
+                                        <input className='rounded-3xl px-4 py-2' type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+                                    </label>
+                                )}
+                                <br />
+                                {!isRegistering && (
+                                    <a href="#" className='text-slate-700 hover:text-white transition'>¿No recuerda su contraseña?</a>
+                                )}
+                                <br />
+                                <br />
+                                {err && <div className='text-red-500'>{err}</div>}
+                                <button type="submit" className="rounded-full bg-red-500 text-white px-4 py-2 w-full hover:bg-red-800 transition">
+                                    {isRegistering ? 'Registrar' : 'Login'}
+                                </button>
+                                <div className='text-slate-700'>
+                                    <a href="#" onClick={toggleForm} className='hover:text-white transition'>
+                                        {isRegistering ? '¿Ya tienes cuenta? Inicia sesión' : '¿No tienes cuenta? Regístrate'}
+                                    </a>
+                                </div>
+                            </form>
+                        </motion.div>
                     </div>
-
                 </div>
                 <div className='my-5'>
                     <img src={coco} alt="Image" className="rounded-3xl object-cover relative top-[-2px] right-[-40px]" />
                 </div>
-
-                
             </div>
         </div>
-        </>
     );
 }
--- webSenati/src/components/Sidebar.jsx
+++ webSenati/src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ export default function Sidebar() {
         
             <div className='flex flex-col items-center justify-center h-2/7 mb-10'>
                 <Link to="/home">
-                    <div className='w-24 h-24 mt-2 flex flex-col items-center justify-center hover:bg-slate-100 rounded-full'>
+                    <div className='w-24 h-24 mt-2 flex flex-col items-center justify-center hover:bg-slate-100 rounded-full transition'>
                         <img className='w-20 h-20 rounded-full ' src="/logococo.svg" alt="" />
                     </div>
                 </Link>
@@ -37,21 +37,21 @@ export default function Sidebar() {
             </div>
             <div className='h-5/7 overflow-auto '>
                 <Link to="/home">
-                    <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full'>
+                    <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full transition'>
                         <h1 className='text-2xl font-bold'>Inicio</h1>
                     </div>
                 </Link>
                 <Link to="/home/buscar">
-                    <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full'>
+                    <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full transition'>
                         <h1 className='text-2xl font-bold'>Buscar</h1>
                     </div>
                 </Link>
                 <Link to="/home/miperfil">
-                    <div className='flex flex-col items-center justify-center h-16 mb-96 group/item hover:bg-slate-100 rounded-full'>
+                    <div className='flex flex-col items-center justify-center h-16 mb-96 group/item hover:bg-slate-100 rounded-full transition'>
                         <h1 className='text-2xl font-bold'>Perfil</h1>
                     </div>
                 </Link>
-                <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full cursor-pointer' onClick={handleClick}>
+                <div className='flex flex-col items-center justify-center h-16 group/item hover:bg-slate-100 rounded-full cursor-pointer transition' onClick={handleClick}>
                     <h1 className='text-2xl font-bold'>Cerrar Sesión</h1>
                 </div>
             </div>
